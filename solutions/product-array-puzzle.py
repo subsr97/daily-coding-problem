9e5c60312aeb57bdc39f17eb2340a73b69a0d7c0
Subramanian <subsr97@gmail.com>
2018-07-17 15:24:02 +0530

    Added product-array-puzzle.py

---
+++ solutions/product-array-puzzle.py
@@ -0,0 +1,43 @@
+# Product except i-th element
+
+def bruteforce(arr):
+    l = len(arr)
+    prod = []
+    for i in range(l):
+        product = 1
+        for j in range(l):
+            if i != j:
+                product *= arr[j]
+        prod.append(product)
+    return prod
+
+def usingDivision(arr):
+    product = 1
+    for number in arr:
+        product *= number
+
+    prod = [product//x for x in arr]
+    return prod
+
+def withoutdivision(arr):
+    l = len(arr)
+    left = [1 for _ in range(l)]
+    right = [1 for _ in range(l)]
+    prod = [1 for _ in range(l)]
+
+    for i in range(1,l):
+        left[i] = arr[i-1] * left[i-1]
+
+    for i in range(l-2, -1, -1):
+        right[i] = arr[i+1] * right[i+1]
+
+    for i in range(l):
+        prod[i] = left[i] * right[i]
+
+    return prod
+
+if __name__ == "__main__":
+    arr = [int(x) for x in input().strip().split()]
+    print(bruteforce(arr))
+    print(usingDivision(arr))
+    print(withoutdivision(arr))
