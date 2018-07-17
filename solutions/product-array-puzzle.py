4849fe0fffe38ad696f0638b93297ba1a9aa08ba
Subramanian <subsr97@gmail.com>
2018-07-17 15:32:43 +0530

    Wrapped the question

--- solutions/product-array-puzzle.py
+++ solutions/product-array-puzzle.py
@@ -1,5 +1,6 @@
 """
-Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the 
+numbers in the original array except the one at i.
 """
 
 def bruteforce(arr):
