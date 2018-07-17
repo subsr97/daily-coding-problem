b69de38efdd1be512c6131cb9a3f5823a651e24c
Subramanian <subsr97@gmail.com>
2018-07-17 15:28:46 +0530

    Added bruteforce

--- solutions/sum-pair-in-array.py
+++ solutions/sum-pair-in-array.py
@@ -1,3 +1,13 @@
+# Find if a pair of numbers exist in a list that add upto the required sum
+
+def bruteforce(numbers, neededSum):
+    l = len(numbers)
+    for i in range(l-1):
+        for j in range(i+1,l):
+            if numbers[i]+numbers[j] == neededSum:
+                return True
+    return False
+
 def checkSumPair(numbers, neededSum):
     knownNumbers = set()
     for number in numbers:
