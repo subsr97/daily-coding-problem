aceef158dc39520dd1ce298d22aa71fcbde814cd
Subramanian <subsr97@gmail.com>
2018-07-17 23:47:01 +0530

    started #3

---
+++ solutions/serialize-and-deserialize.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.val)
+
+n = Node(4)
+print(n)
