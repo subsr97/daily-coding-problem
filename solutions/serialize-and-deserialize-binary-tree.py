0152b902c12b772964e3eb08ef777e88b8fb10ab
Subramanian <subsr97@gmail.com>
2018-07-20 17:39:13 +0530

    Update serialize-and-deserialize-binary-tree.py
    Add question

--- solutions/serialize-and-deserialize-binary-tree.py
+++ solutions/serialize-and-deserialize-binary-tree.py
@@ -1,3 +1,21 @@
+"""
+#3
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+"""
 import json
 
 class Node:
