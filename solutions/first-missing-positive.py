526507060ec25d05e1f9585c59e804997b91f35e
Subramanian <subsr97@gmail.com>
2018-07-20 10:02:08 +0530

    Create first-missing-positive.py

---
+++ solutions/first-missing-positive.py
@@ -0,0 +1,38 @@
+"""
+Given an array of integers, find the first missing positive integer in linear time and constant space.
+In other words, find the lowest positive integer that does not exist in the array.
+The array can contain duplicates and negative numbers as well.
+"""
+
+# Returns a slice containing positive numbers
+def findPositiveSubArr(arr):
+
+    negativeIndex = 0
+
+    for  i in range(len(arr)):
+        if arr[i] <= 0:
+            arr.insert(negativeIndex, arr.pop(i))
+            negativeIndex += 1
+
+    return arr[negativeIndex:]
+
+# Returns the first missing positive number
+def findMissingPositive(positiveArr):
+    l = len(positiveArr)
+    for num in positiveArr:
+        index = abs(num)-1
+        if index < l and positiveArr[index]>0:
+            positiveArr[index] *= -1
+
+    for i in range(l):
+        if positiveArr[i] > 0:
+            return i+1
+
+    return l+1
+
+if __name__ == "__main__":
+    arr = [int(x) for x in input().strip().split()]
+
+    positiveSubArr = findPositiveSubArr(arr)
+
+    print(findMissingPositive(positiveSubArr))
