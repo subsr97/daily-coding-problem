f1337b07593390006bdcc2564fb8e1ca3022d4cf
Subramanian <subsr97@gmail.com>
2018-07-20 17:42:51 +0530

    Update product-array-puzzle.py
    Add company

--- solutions/product-array-puzzle.py
+++ solutions/product-array-puzzle.py
@@ -1,5 +1,6 @@
 """
 #2
+Uber
 
 Given an array of integers, return a new array such that each element at index i of the new array is the product of 
 all the numbers in the original array except the one at i.
