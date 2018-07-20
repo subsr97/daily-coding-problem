604b924a98b127b6d6f3cbcc250a992bad07efe8
Subramanian <subsr97@gmail.com>
2018-07-20 17:42:24 +0530

    Update serialize-and-deserialize-binary-tree.py
    Add company

--- solutions/serialize-and-deserialize-binary-tree.py
+++ solutions/serialize-and-deserialize-binary-tree.py
@@ -1,5 +1,6 @@
 """
 #3
+Google
 
 Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
 
