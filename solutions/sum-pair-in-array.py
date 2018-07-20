1fc4b88619754ccb5c2b93714e9d60e2027c6e98
Subramanian <subsr97@gmail.com>
2018-07-20 17:38:14 +0530

    Update sum-pair-in-array.py
    Update question

--- solutions/sum-pair-in-array.py
+++ solutions/sum-pair-in-array.py
@@ -1,7 +1,10 @@
 """
+#1
+
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
 """
 
 def bruteforce(numbers, neededSum):
