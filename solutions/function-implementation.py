dcfde18a9c267cf951eda21df165c5d09b87b31b
Subramanian <subsr97@gmail.com>
2018-07-21 00:22:01 +0530

    Update function-implementation.py
    Add line break

--- solutions/function-implementation.py
+++ solutions/function-implementation.py
@@ -2,7 +2,8 @@
 #5
 Jane Street
 
-cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 
 Given this implementation of cons:
 
