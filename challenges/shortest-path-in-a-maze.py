8afd1fb807a077704aa56e89969b6be0ecbbff8f
Subramanian <subsr97@gmail.com>
2018-08-07 10:09:33 +0530

    Update #23

--- challenges/shortest-path-in-a-maze.py
+++ challenges/shortest-path-in-a-maze.py
@@ -5,7 +5,9 @@ Google
 You are given an M by N matrix consisting of booleans that represents a board.
 Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
 
-Given this matrix, a start coordinate, and an end coordinate, return the minimum number of steps required to reach the end coordinate from the start.
+Given this matrix, a start coordinate, and an end coordinate, return the minimum number of steps
+required to reach the end coordinate from the start.
+
 If there is no possible path, then return null. You can move up, left, down, and right.
 You cannot move through walls. You cannot wrap around the edges of the board.
 
@@ -55,5 +57,8 @@ if __name__ == "__main__":
 
     findshortestDistance(maze, start, end, [], 0)
 
-    print(shortestDistance)    
+    if shortestDistance != math.inf:
+        print(shortestDistance)
+    else:
+        print(None)
     
