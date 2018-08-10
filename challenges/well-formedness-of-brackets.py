1fd8235cdb4dd896ad7de5ee802a39adefa637d9
Subramanian <clawsonfire97@gmail.com>
2018-08-10 23:31:52 +0530

    Add #26

---
+++ challenges/well-formedness-of-brackets.py
@@ -0,0 +1,35 @@
+"""
+#27
+Facebook
+
+Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false.
+
+"""
+def checkWellFormedness(bracketString):
+    bracketStack = []
+    openingBrackets = {"(", "{", "["}
+
+    matchingBracketsDict = {")":"(", "]":"[", "}":"{"}
+
+    for bracket in bracketString:
+        if bracket in openingBrackets:
+            bracketStack.append(bracket)
+        else:
+            if bracketStack[-1] == matchingBracketsDict[bracket]:
+                bracketStack.pop()
+            else:
+                return False
+    
+    return not bracketStack
+
+def main():
+    print(checkWellFormedness("([])[]({})"))
+    print(checkWellFormedness("([)]"))
+    print(checkWellFormedness("((()"))
+
+if __name__ == "__main__":
+    main()
