29796c57168028c2d07c4e386acf39324fbfe026
Subramanian Sridharan <subsr97@gmail.com>
2019-01-27 14:34:33 +0530

    Update two-d-iterator.py

--- challenges/two-d-iterator.py
+++ challenges/two-d-iterator.py
@@ -6,7 +6,6 @@ Implement a 2D iterator class. It will be initialized with an array of arrays, a
 
 next(): returns the next element in the array of arrays. If there are no more elements, raise an exception.
 has_next(): returns whether or not the iterator still has elements left.
-
 For example, given the input [[1, 2], [3], [], [4, 5, 6]], calling next() repeatedly should output 1, 2, 3, 4, 5, 6.
 
 Do not use flatten or otherwise clone the arrays. Some of the arrays can be empty.
@@ -52,7 +51,10 @@ def main():
     while twoDIterator.has_next():
         print(twoDIterator.next(), end=" ")
     
+    # 1 2 3 4 5 6
+
     print()
+
 
     twoDIterator = TwoDIterator([[1, 2], [3], [], [4, 5, 6]])
 
@@ -62,7 +64,9 @@ def main():
         except Exception as e:
             print(e)
             break
-            
+    
+    # 1 2 3 4 5 6 list index out of range
+    
 
 if __name__ == "__main__":
     main()
